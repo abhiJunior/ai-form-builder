9978bacca48b76e9cf553e758116e5c7a6cb51ba
Abhishek <149757033+abhiJunior@users.noreply.github.com>
2025-10-20 01:16:40 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { Share, Copy, X } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 function Dashboard() {
-  const url = "http://localhost:5000"
+  const url = "https://ai-form-builder-backend-liwf.onrender.com"
   const [forms, setForms] = useState([]);
   const [loading, setLoading] = useState(true);
   const [shareModal, setShareModal] = useState({ open: false, link: '' });
