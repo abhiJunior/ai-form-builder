4a72f0bfd8156063d9b6ac264ba5ee93eed9b4d0
Abhishek <149757033+abhiJunior@users.noreply.github.com>
2025-10-22 08:57:20 +0530

    Update LiveForm.jsx

--- client/src/pages/LiveForm.jsx
+++ client/src/pages/LiveForm.jsx
@@ -7,7 +7,7 @@ const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 const CLOUDINARY_PRESET = import.meta.env.VITE_CLOUDINARY_PRESET;
 
 function LiveForm() {
-  const url = "https://ai-form-builder-backend-liwf.onrender.com"
+  const url = "https://ai-form-builder-backend-ytpx.onrender.com"
   const { id: formId } = useParams();
   const [formData, setFormData] = useState(null);
   const [formValues, setFormValues] = useState({});
